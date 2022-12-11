c890628678ea9e59f40036ab72e46a52adfbff70
celassad <chaimaa.elassad@gmail.com>
2022-12-11 19:13:14 +0100

    Add request post photo

---
+++ helpers.js
@@ -0,0 +1,99 @@
+const { 
+    v1: uuidv1,
+    v4: uuidv4,
+  } = require('uuid');
+const fs = require('mz/fs');
+
+function sendSuccessfulResponse(res, msg, status=200){
+    let jsonResponse = {
+        ok: true,
+        message: msg,
+    }
+
+    res.status(status).json(jsonResponse)
+}
+
+function sendFailedResponse(res, msg, status=500){
+    let jsonResponse = {
+        ok: false,
+        message: msg
+    }
+
+    res.status(status).json(jsonResponse)
+}
+
+  
+function splitImageName(name){
+    return {name : name.split('.')[0], format: name.split('.')[1]}
+}
+
+function serverImageName(imageName){
+    const {name, format} = splitImageName(imageName)
+    const date = new Date(Date.now()).toJSON()
+    const serverFileName = `ANTS_${name}_${date}.${format}`
+    return serverFileName
+}
+
+function addPhotoToDisk(res, data, filePath){
+    let buff = Buffer.from(data, 'base64');
+      fs.writeFile(filePath, buff, (err) => {
+        if (err) {
+            sendFailedResponse(res, err)
+            console.log(err)
+        } else {
+            const message = "File written successfully"
+            sendSuccessfulResponse(res, message)
+            console.log(message)
+        }
+      });
+  }
+  
+
+function addPhotoToDB(photo){
+
+    fs.readFile('photos.json', 'utf8', function readFileCallback(err, data){
+      if (err){
+          console.log(err);
+      } else {
+      newEntry = {
+        id:uuidv4(),
+        name: photo.name,
+        fileSize: photo.fileSize,
+        width: photo.width,
+        height: photo.height,
+        date: photo.date,
+        syncDate: new Date(Date.now()).toJSON()
+      }
+      obj = JSON.parse(data); //now it an object
+      obj.photos.push(newEntry); //add some data
+      json = JSON.stringify(obj); //convert it back to json
+      fs.writeFile('photos.json', json, 'utf8'); // write it back 
+    }});
+  
+  }
+
+  function areEqual(p, photo){
+    if(p.name === photo.name && p.date === photo.date){
+        return true
+    } 
+    return false
+  }
+
+  function isPhotoInDB(photo){
+
+    data = fs.readFileSync('photos.json', 'utf8');
+    obj = JSON.parse(data); 
+    isInDB = obj.photos.some((p)=>areEqual(p, photo))
+
+    return isInDB
+  }
+
+module.exports = {
+    sendSuccessfulResponse,
+    sendFailedResponse,
+    splitImageName,
+    serverImageName,
+    addPhotoToDisk,
+    addPhotoToDB,
+    isPhotoInDB
+};
--- testExpress.js
+++ testExpress.js
@@ -1,5 +1,7 @@
+const helpers = require('./helpers');
 const express = require('express')
 const fs = require('mz/fs');
+const bodyParser = require('body-parser');
 
 const app = express()
 const host = '192.168.0.21';
@@ -9,14 +11,34 @@ const rootPath = '/home/chaimaa/Ants/Photos/'
 const idToPathArray = [['1', 'image1.jpg'], ['2', 'image2.jpg']];
 const idToPath = new Map(idToPathArray)
 
+app.use(bodyParser.json({limit: '50mb'}));
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/photo', (req,res) => {
+
+  if(helpers.isPhotoInDB(req.body)){
+
+    const msg = "Photo already added to server."
+    console.log(msg)
+    helpers.sendFailedResponse(res, msg, 409)
+
+  }else{
+
+    const serverFilePath = rootPath + helpers.serverImageName(req.body.name)
+    helpers.addPhotoToDB(req.body)
+    helpers.addPhotoToDisk(res, req.body.image64, serverFilePath)   
+
+  }
+
 })
 
 app.get('/photo/:id', (req, res) => {
     
     var imagePath = idToPath.get(req.params['id'])
-    
+
     fs.readFile(rootPath + imagePath).then((data) => {
         res.json({image64 : `${data.toString('base64')}`})
     })
