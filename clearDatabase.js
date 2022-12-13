16667dd4510c7436183ade049d943abba29053a2
Issam <issambenelgada@gmail.com>
2022-12-13 23:05:38 +0100

    Added clearing photo directory to clearDatabase.js

--- clearDatabase.js
+++ clearDatabase.js
@@ -1,13 +1,26 @@
 // IMPORTS
-const fs = require('mz/fs');
-const config = require('./config')
+const path = require("node:path");
+const fs = require("mz/fs");
+const config = require("./config");
 
 // CONFIG
-const DBFile = config.DBFile
+const DBFile = config.DBFile;
 
 const obj = {
-    photos: []
-}
-const str = JSON.stringify(obj)
-fs.writeFile(DBFile, str, 'utf8');
-console.log("Database cleared.")
+  photos: [],
+};
+const str = JSON.stringify(obj);
+fs.writeFile(DBFile, str, "utf8");
+console.log("Database cleared.");
+
+fs.readdir(config.rootPath, (err, files) => {
+  if (err) throw err;
+
+  for (const file of files) {
+    fs.unlink(path.join(config.rootPath, file), (err) => {
+      if (err) throw err;
+    });
+  }
+});
+
+console.log(config.rootPath + " directory cleared.");
