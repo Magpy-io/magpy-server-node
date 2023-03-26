5af6206f914147987c53d3202f16c158093229de
Issam <issambenelgada@gmail.com>
2023-03-26 15:19:04 +0200

    Changed config.

--- src/config/config.js
+++ src/config/config.js
@@ -3,8 +3,8 @@ const port = 8000;
 const rootPath = "/home/issam/Documents/Images/";
 const sqliteDbFile = __srcdir + "/db/database.db";
 const hashLen = 32;
-const postPhotoPartTimeout = 100; //in ms
-const getPhotoPartSize = 10; //char in base64
+const postPhotoPartTimeout = 2000; //in ms
+const getPhotoPartSize = 100000; //char in base64
 
 module.exports = {
   host,
