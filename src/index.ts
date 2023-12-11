a77fb8f51ce80bcf1342443b0aaa51651c3a9716
Issam <issambenelgada@gmail.com>
2023-12-11 18:01:43 +0100

    Added testing framework and a fist test.

--- src/index.ts
+++ src/index.ts
@@ -2,37 +2,16 @@
 require("module-alias/register");
 
 // IMPORTS
-import express from "express";
-import bodyParser from "body-parser";
-
-import { port, serverMdnsName } from "@src/config/config";
-import loadEndpoints from "@src/api/endpointsLoader";
+import { initServer, startMdns } from "@src/server/server";
 import { initDB } from "@src/db/databaseFunctions";
-import mdns from "mdns";
 
 main().catch((err) => {
-  console.log("error in main");
+  console.log("error init server");
   console.log(err);
 });
 
 async function main() {
   await initDB();
-
-  console.log("Database initialized.");
-
-  const app = express();
-
-  app.use(bodyParser.json({ limit: "50mb" }));
-
-  loadEndpoints(app);
-  console.log("Endpoints loaded");
-
-  app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
-  });
-
-  const advert = mdns.createAdvertisement(mdns.tcp("http"), port, {
-    name: serverMdnsName,
-  });
-  advert.start();
+  initServer();
+  startMdns();
 }
