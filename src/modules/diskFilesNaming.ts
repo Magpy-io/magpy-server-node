fcddf4d99f50f9229ce6ad37e39162067db54c25
Issam <issambenelgada@gmail.com>
2024-01-05 00:24:52 +0100

    Addeds search for first available file name.

--- src/modules/diskFilesNaming.ts
+++ src/modules/diskFilesNaming.ts
@@ -2,6 +2,7 @@ import { Photo } from "@src/types/photoType";
 import * as path from "path";
 
 import { GetStorageFolderPath } from "@src/modules/serverDataManager";
+import { pathExists } from "./diskManager";
 
 function splitImageName(fullName: string) {
   const nameSplited = fullName.split(".");
@@ -14,9 +15,22 @@ async function createServerImageName(photo: Photo) {
   const dirPath = await GetStorageFolderPath();
   const { name, format } = splitImageName(photo.name);
   const date = photo.syncDate;
-  const dateFormated = date.replace(/\:/g, "-");
+  const dateFormated = ""; //date.replace(/\:/g, "-");
   const serverFileName = `Ants_${name}_${dateFormated}.${format}`;
-  return path.join(dirPath, serverFileName);
+  const newPhotoPath = path.join(dirPath, serverFileName);
+
+  let i = 0;
+  let newPhotoPath_i = newPhotoPath;
+
+  while (await pathExists(newPhotoPath_i)) {
+    i++;
+    const newPhotoPathParsed = path.parse(newPhotoPath);
+    newPhotoPath_i = path.join(
+      newPhotoPathParsed.dir,
+      newPhotoPathParsed.name + `_${i}` + newPhotoPathParsed.ext
+    );
+  }
+  return newPhotoPath_i;
 }
 
 function createServerImageThumbnailName(fullImagePath: string) {
