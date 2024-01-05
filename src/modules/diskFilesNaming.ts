2d9737adfb292371a1435e975b1aaa9d311ff8f9
Issam <issambenelgada@gmail.com>
2024-01-05 02:02:23 +0100

    Added add increment to fileName if file with same name exists.

--- src/modules/diskFilesNaming.ts
+++ src/modules/diskFilesNaming.ts
@@ -2,26 +2,30 @@ import { Photo } from "@src/types/photoType";
 import * as path from "path";
 
 import { GetStorageFolderPath } from "@src/modules/serverDataManager";
+import { pathExists } from "@src/modules/diskManager";
 
 async function createServerImageName(photo: Photo) {
   const dirPath = await GetStorageFolderPath();
   const nameParsed = path.parse(photo.name);
   const serverFileName = createBaseName(photo) + nameParsed.ext;
-  return path.join(dirPath, serverFileName);
+  const filePath = path.join(dirPath, serverFileName);
+  return await getFirstAvailableFileName(filePath);
 }
 
 async function createServerImageThumbnailName(photo: Photo) {
   const dirPath = await GetStorageFolderPath();
   const nameParsed = path.parse(photo.name);
   const serverFileName = createBaseName(photo) + "_thumbnail" + nameParsed.ext;
-  return path.join(dirPath, serverFileName);
+  const filePath = path.join(dirPath, serverFileName);
+  return await getFirstAvailableFileName(filePath);
 }
 
 async function createServerImageCompressedName(photo: Photo) {
   const dirPath = await GetStorageFolderPath();
   const nameParsed = path.parse(photo.name);
   const serverFileName = createBaseName(photo) + "_compressed" + nameParsed.ext;
-  return path.join(dirPath, serverFileName);
+  const filePath = path.join(dirPath, serverFileName);
+  return await getFirstAvailableFileName(filePath);
 }
 
 function createBaseName(photo: Photo) {
@@ -36,6 +40,21 @@ async function addServerImagePaths(photo: Photo) {
   photo.serverPath = await createServerImageName(photo);
   photo.serverCompressedPath = await createServerImageCompressedName(photo);
   photo.serverThumbnailPath = await createServerImageThumbnailName(photo);
+}
+
+async function getFirstAvailableFileName(filePath: string) {
+  let i = 0;
+  let filePath_i = filePath;
+  const filePathParsed = path.parse(filePath);
+
+  while (await pathExists(filePath_i)) {
+    i++;
+    filePath_i = path.join(
+      filePathParsed.dir,
+      filePathParsed.name + `_${i}` + filePathParsed.ext
+    );
+  }
+  return filePath_i;
 }
 
 export { addServerImagePaths };
