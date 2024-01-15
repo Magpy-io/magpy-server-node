47eb7bba741bc086b4e4e79e9f6d251a5e34eacb
Issam <issambenelgada@gmail.com>
2024-01-15 22:42:21 +0100

    Added script for coping types to export.

--- src/CopyEndpointTypesToExport.ts
+++ src/CopyEndpointTypesToExport.ts
@@ -3,12 +3,18 @@ import { join, parse } from "path";
 import fs from "fs/promises";
 
 async function copyFiles(): Promise<void> {
-  const inputPath = "./src/api/types/";
-  const outputPath = "./src/api/export/types/";
+  try {
+    const inputPath = "./src/api/types/";
+    const outputPath = "./src/api/export/types/";
 
-  await fs.rm(outputPath, { force: true, recursive: true });
+    await fs.rm(outputPath, { force: true, recursive: true });
 
-  await fs.cp(inputPath, outputPath, { recursive: true });
+    await fs.cp(inputPath, outputPath, { recursive: true });
+    console.log("Types copied successfully to export folder");
+  } catch (err) {
+    console.log("Error copying types folder to export");
+    console.log(err);
+  }
 }
 
 copyFiles();
