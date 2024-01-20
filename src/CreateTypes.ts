f8fb9749989117349a196bcd7eabb48466a5a496
Issam <issambenelgada@gmail.com>
2024-01-20 15:49:19 +0100

    Updated types for export generation to remove joi Schemas.

--- src/CreateTypes.ts
+++ src/CreateTypes.ts
@@ -2,6 +2,14 @@ import { convertFromDirectory } from "joi-to-typescript";
 import { glob } from "glob";
 import { join, parse } from "path";
 import fs from "fs/promises";
+import {
+  AllExport,
+  TypeAliasDeclaration,
+  TypescriptParser,
+  VariableDeclaration,
+} from "typescript-parser";
+
+const parser = new TypescriptParser();
 
 async function types(): Promise<void> {
   const inputPath = "./src/api/Types/EndpointsApi";
@@ -21,6 +29,9 @@ async function types(): Promise<void> {
   // once the types are generated
   for (let tsfile of tsfiles) {
     const fileName = parse(tsfile).base;
+
+    verifyFileDeclarations(tsfile);
+
     let dir = parse(tsfile).dir;
     let pathRecursive = "";
     while (dir != inputPathAbsolute) {
@@ -51,6 +62,98 @@ async function types(): Promise<void> {
   } else {
     // eslint-disable-next-line no-console
     console.log("Failed to run joi-to-typescript");
+  }
+}
+
+async function verifyFileDeclarations(filePath: string) {
+  const fileName = parse(filePath).name;
+
+  const fileStr = await fs.readFile(filePath, { encoding: "utf-8" });
+
+  const parsed = await parser.parseSource(fileStr);
+
+  const responseDataType = parsed.declarations.find(
+    (e) => e.name == "ResponseData"
+  );
+  const RequestShemaObject = parsed.declarations.find(
+    (e) => e.name == "RequestSchema"
+  );
+  const endpointName = parsed.declarations.find((e) => e.name == "endpoint");
+  const tokenAuth = parsed.declarations.find((e) => e.name == "tokenAuth");
+  const exportAll = parsed.exports.find((e) => e instanceof AllExport);
+
+  if (
+    !responseDataType ||
+    !(responseDataType instanceof TypeAliasDeclaration)
+  ) {
+    throw new Error(
+      "ResponseData type must be exported in file '" + filePath + "'"
+    );
+  }
+
+  if (
+    !RequestShemaObject ||
+    !(RequestShemaObject instanceof VariableDeclaration)
+  ) {
+    throw new Error(
+      "RequestSchema object must be defined and exported as a 'Joi.object' in file '" +
+        filePath +
+        "'"
+    );
+  }
+
+  if (!endpointName || !(endpointName instanceof VariableDeclaration)) {
+    throw new Error(
+      "endpoint name string must be exported with same name as file in file '" +
+        filePath +
+        "'\nexample : 'export const endpoint = '" +
+        fileName +
+        "';'"
+    );
+  }
+
+  try {
+    const endpointString = fileStr.substring(
+      endpointName.start as number,
+      endpointName.end
+    );
+
+    const endpointStringSplit = endpointString.split(/['"]/);
+
+    if (!endpointStringSplit.includes(fileName)) {
+      throw new Error();
+    }
+  } catch (err) {
+    console.log(err);
+    throw new Error(
+      "endpoint name string must be exported with same name as file in file '" +
+        filePath +
+        "'\nexample : 'export const endpoint = '" +
+        fileName +
+        "';'"
+    );
+  }
+
+  if (!tokenAuth || !(tokenAuth instanceof VariableDeclaration)) {
+    throw new Error(
+      "tokenAuth of type TokenAuthentification must be declared and exported in file '" +
+        filePath +
+        "'\nexample : 'export const tokenAuth: TokenAuthentification = 'yes';"
+    );
+  }
+
+  if (
+    !exportAll ||
+    !(exportAll instanceof AllExport) ||
+    exportAll.from != "../RequestTypes/" + fileName
+  ) {
+    throw new Error(
+      "An export needs to be added with the auto-generated type for the RequestSchema in file '" +
+        filePath +
+        "'\nexample : 'export * from '../RequestTypes/" +
+        fileName +
+        ";'"
+    );
   }
 }
 
