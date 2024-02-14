73392411e9fe4f4f90017d983fa54b35e0cce556
Issam Benelgada <issambenelgada@gmail.com>
2024-02-15 00:56:20 +0100

    Changed ts and jest config to remove tests from build in dist/ folder.

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,19 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  coveragePathIgnorePatterns: ["/node_modules/"],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  coveragePathIgnorePatterns: ['/node_modules/'],
   moduleNameMapper: {
-    "^@src/(.*)$": "<rootDir>/src/$1",
-    "^@tests/(.*)$": "<rootDir>/tests/$1",
+    '^@src/(.*)$': '<rootDir>/src/$1',
+    '^@tests/(.*)$': '<rootDir>/tests/$1',
   },
-  modulePathIgnorePatterns: ["<rootDir>/dist/", "<rootDir>/client"],
+  modulePathIgnorePatterns: ['<rootDir>/dist/', '<rootDir>/client'],
+  transform: {
+    '^.+\\.[tj]sx?$': [
+      'ts-jest',
+      {
+        tsconfig: './tests/tsconfig.json',
+      },
+    ],
+  },
 };
