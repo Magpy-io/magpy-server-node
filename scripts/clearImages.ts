6fff22bce4edb05cdcf1a426c2f03bbfb9fbedd0
Issam Benelgada <issambenelgada@gmail.com>
2024-02-15 20:27:37 +0100

    Fixed yarn clear:images script.

--- scripts/clearImages.ts
+++ scripts/clearImages.ts
@@ -1,18 +1,17 @@
 // IMPORTS
 import dotenv from 'dotenv';
 
-import { ClearModulesKeepServerConfig, InitModules } from '../src/config/configModules';
 import { clearDbFile, clearImagesDisk } from '../src/modules/diskManager';
+import { LoadConfigFile } from '../src/modules/serverDataManager';
 
 dotenv.config();
 
 async function ResetServer() {
-  await InitModules();
+  await LoadConfigFile();
   await clearDbFile();
   console.log('database cleared');
   await clearImagesDisk();
   console.log('storage directory cleared.');
-  await ClearModulesKeepServerConfig();
 }
 
 ResetServer().catch(err => {
