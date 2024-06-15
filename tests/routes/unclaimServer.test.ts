29e2c507bb55597ca4695fef57db85b2d53b426d
Issam Benelgada <issambenelgada@gmail.com>
2024-06-15 23:00:42 +0200

    Added test for last commit.

--- tests/routes/unclaimServer.test.ts
+++ tests/routes/unclaimServer.test.ts
@@ -5,7 +5,7 @@ mockModules();
 import { describe, expect, it } from '@jest/globals';
 
 import { Express } from 'express';
-import { UnclaimServer } from '@src/api/export';
+import { UnclaimServer, GetToken, WhoAmI } from '@src/api/export';
 
 import { initServer, stopServer } from '@src/server/server';
 
@@ -18,7 +18,14 @@ import {
   IsServerClaimedLocal,
   IsServerClaimedRemote,
 } from '@src/modules/serverDataManager';
-import { expectToBeOk, setupServerClaimed, setupServerClaimedLocally } from '@tests/helpers/functions';
+import {
+  expectErrorCodeToBe,
+  expectToBeOk,
+  expectToNotBeOk,
+  setupServerClaimed,
+  setupServerClaimedLocally,
+} from '@tests/helpers/functions';
+import { validUserToken } from '@src/modules/BackendQueries/__mocks__/mockValues';
 
 describe("Test 'unclaimServer' endpoint", () => {
   let app: Express;
@@ -40,7 +47,7 @@ describe("Test 'unclaimServer' endpoint", () => {
   });
 
   it('Should return ok if unclaiming a remotly claimed server', async () => {
-    await setupServerClaimed()
+    await setupServerClaimed();
     const ret = await UnclaimServer.Post();
 
     expectToBeOk(ret);
@@ -56,7 +63,7 @@ describe("Test 'unclaimServer' endpoint", () => {
   });
 
   it('Should return ok if unclaiming a locally claimed server', async () => {
-    await setupServerClaimedLocally()
+    await setupServerClaimedLocally();
     const ret = await UnclaimServer.Post();
 
     expectToBeOk(ret);
@@ -66,5 +73,20 @@ describe("Test 'unclaimServer' endpoint", () => {
 
     expect(IsServerClaimedLocal()).toBe(false);
     expect(serverCredentials).toBeFalsy();
+  });
+
+  it('Should invalidate any previosly issued tokens after unclaim', async () => {
+    await setupServerClaimed();
+
+    await GetToken.Post({ userToken: validUserToken });
+
+    const ret = await UnclaimServer.Post();
+
+    await setupServerClaimed();
+
+    const retWhoAmI = await WhoAmI.Post();
+
+    expectToNotBeOk(retWhoAmI);
+    expectErrorCodeToBe(retWhoAmI, 'AUTHORIZATION_FAILED');
   });
 });
