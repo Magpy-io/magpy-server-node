f27a6ff0dbe2243c1508216bea4a8af302164816
Issam Benelgada <issambenelgada@gmail.com>
2024-08-05 14:57:59 +0200

    Testing new solutions for bundling.

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,21 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   target: 'node',
   entry: './dist/index.js',
+  devtool: 'source-map',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'bundle'),
   },
-  optimization: {
-    usedExports: false,
-  },
   externals: {
-    sharp: 'commonjs sharp',
-    sqlite3: 'commonjs sqlite3',
+    'E:/Libraries/Documents/Projects/opencloud-server/node_modules/sharp/build/Release/sharp-win32-x64.node':
+      'sharp_win32_x64_node',
+    'E:/Libraries/Documents/Projects/opencloud-server/node_modules/sqlite3/build/Release/node_sqlite3.node':
+      'node_sqlite3_node',
   },
   plugins: [
     new CopyPlugin({
@@ -24,4 +25,16 @@ module.exports = {
       ],
     }),
   ],
+  optimization: {
+    minimize: false,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          parse: {
+            bare_returns: true, //allow code with returns outside of functions, solved minimizing error.
+          },
+        },
+      }),
+    ],
+  },
 };
