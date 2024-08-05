c6c650650f2b464d335f9a03610c9a810a865479
Issam Benelgada <issambenelgada@gmail.com>
2024-08-05 13:41:37 +0200

    Patched sequelize to get bundled correctly.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -8,8 +9,19 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'bundle'),
   },
+  optimization: {
+    usedExports: false,
+  },
   externals: {
     sharp: 'commonjs sharp',
     sqlite3: 'commonjs sqlite3',
   },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: 'node_modules/sharp', to: 'node_modules/sharp' },
+        { from: 'node_modules/sqlite3', to: 'node_modules/sqlite3' },
+      ],
+    }),
+  ],
 };
