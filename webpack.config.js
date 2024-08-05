81156869cd23c56a615474cf6e118da626277b92
Issam Benelgada <issambenelgada@gmail.com>
2024-08-05 03:19:09 +0200

    Changed webpack config to exlude sharp and sqlite3.

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,15 @@
 const path = require('path');
 
 module.exports = {
-  mode:'production',
+  mode: 'development',
   target: 'node',
   entry: './dist/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'bundle'),
   },
-  module: {
-    rules: [
-      {
-        test: /\.node$/,
-        loader: 'node-loader',
-        options: {
-          name: '[path][name].[ext]',
-        },
-      },
-    ],
+  externals: {
+    sharp: 'commonjs sharp',
+    sqlite3: 'commonjs sqlite3',
   },
 };
