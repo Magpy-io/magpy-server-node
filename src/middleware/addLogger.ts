b424a077a779ee13d54410a6724367e631080d01
Issam Benelgada <issambenelgada@gmail.com>
2024-10-29 19:57:37 +0100

    Fixed import error.

--- src/middleware/addLogger.ts
+++ src/middleware/addLogger.ts
@@ -1,7 +1,6 @@
 import { NextFunction, Response } from 'express';
 import { ExtendedRequest } from '../types/express/ExtendedRequest';
-import { NewRequestId } from '../modules/RequestIdGenerator';
-import { Logger } from 'src/modules/Logger';
+import { Logger } from '../modules/Logger';
 
 export function addLogger(req: ExtendedRequest, res: Response, next: NextFunction) {
   req.logger = Logger.child({ requestId: req.id });
