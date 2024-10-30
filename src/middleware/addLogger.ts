658897a0694228d1de15d3c9dd69709cfd37f764
Issam Benelgada <issambenelgada@gmail.com>
2024-10-31 00:31:47 +0100

    Added error checking to addLogger middleware.

--- src/middleware/addLogger.ts
+++ src/middleware/addLogger.ts
@@ -1,9 +1,15 @@
 import { NextFunction, Response } from 'express';
 import { ExtendedRequest } from '../types/express/ExtendedRequest';
 import { Logger } from '../modules/Logger';
+import responseFormatter from '../api/responseFormatter';
 
 export function addLogger(req: ExtendedRequest, res: Response, next: NextFunction) {
-  req.logger = Logger.child({ requestId: req.id, url: req.originalUrl });
-  req.logger.http('Request received', { type: 'request' });
-  next();
+  try {
+    req.logger = Logger.child({ requestId: req.id, url: req.originalUrl });
+    req.logger.http('Request received', { type: 'request' });
+    next();
+  } catch (err) {
+    console.error(err);
+    responseFormatter.sendErrorMessage(req, res);
+  }
 }
