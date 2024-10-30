e0ed59153fb04e29eefafaa42272f081fd8762e1
Issam Benelgada <issambenelgada@gmail.com>
2024-10-30 22:33:58 +0100

    Added new http logging level for logging received requests.

--- src/middleware/addLogger.ts
+++ src/middleware/addLogger.ts
@@ -3,6 +3,7 @@ import { ExtendedRequest } from '../types/express/ExtendedRequest';
 import { Logger } from '../modules/Logger';
 
 export function addLogger(req: ExtendedRequest, res: Response, next: NextFunction) {
-  req.logger = Logger.child({ requestId: req.id });
+  req.logger = Logger.child({ requestId: req.id, url: req.originalUrl });
+  req.logger.http('Request received');
   next();
 }
--- src/modules/Logger.ts
+++ src/modules/Logger.ts
@@ -1,18 +1,20 @@
 import winston from 'winston';
 
-const { errors, combine, timestamp, printf } = winston.format;
+const { errors, combine, timestamp } = winston.format;
+
+const logLevels = {
+  error: 0,
+  warn: 1,
+  http: 2,
+  info: 3,
+  debug: 4,
+};
 
 function createLogger() {
   return winston.createLogger({
+    levels: logLevels,
     level: 'debug',
-    format: combine(
-      errors(),
-      timestamp(),
-      printf(
-        info =>
-          `[${info.timestamp}] [${info.requestId || '000'}] ${info.level}: ${info.message}`,
-      ),
-    ),
+    format: combine(errors(), timestamp(), winston.format.json()),
     transports: [new winston.transports.Console()],
   });
 }
