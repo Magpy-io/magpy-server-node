e8eb5afa2c359c70f4aa166fc11a672aabfa6f6c
Issam Benelgada <issambenelgada@gmail.com>
2024-11-12 19:32:57 +0100

    Changed migration files loading.

--- src/db/migrateDb.ts
+++ src/db/migrateDb.ts
@@ -2,21 +2,11 @@ import { Sequelize } from 'sequelize';
 import { Logger } from '../modules/Logger';
 import { SequelizeStorage, Umzug } from 'umzug';
 
-import initial from './migrations/initial';
+import initial from './migrations/20241112183000_00_initial';
 
 export async function migrateDb(sequelize: Sequelize) {
   const umzug = new Umzug({
-    migrations: [
-      {
-        name: '00_initial',
-        async up({ context }) {
-          initial.up({ queryInterface: context });
-        },
-        async down({ context }) {
-          initial.down({ queryInterface: context });
-        },
-      },
-    ],
+    migrations: [initial],
     context: sequelize.getQueryInterface(),
     storage: new SequelizeStorage({ sequelize }),
     logger: Logger.child({ source: 'umzug' }),
