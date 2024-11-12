a4bccf26cfa1ef2fd7edf4e4a9ec51bf3fadd7f7
Issam Benelgada <issambenelgada@gmail.com>
2024-11-12 17:27:21 +0100

    removed unneeded try/catch.

--- src/mainFunction.ts
+++ src/mainFunction.ts
@@ -11,14 +11,10 @@ import { Logger } from './modules/Logger';
 
 export async function main() {
   Logger.info('Running Magpy Server v' + packageJson.version);
-  try {
-    await InitModules();
-    await openAndInitDB();
-    await initServer();
-    setupShutdownManager();
-    await startServerDiscovery();
-    setupOpenInterfaceEvent();
-  } catch (e) {
-    Logger.error('Error starting app', e);
-  }
+  await InitModules();
+  await openAndInitDB();
+  await initServer();
+  setupShutdownManager();
+  await startServerDiscovery();
+  setupOpenInterfaceEvent();
 }
