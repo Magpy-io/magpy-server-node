4c3fe3ed5832dcec566a54c5b9e7ae6f20286012
Issam Benelgada <issambenelgada@gmail.com>
2024-11-21 00:59:26 +0100

    Added performance test for getPhotosByMediaId.

--- tests/performanceTestDB/endpointsPerformance.test.ts
+++ tests/performanceTestDB/endpointsPerformance.test.ts
@@ -3,9 +3,14 @@ import { mockModules } from '@tests/helpers/mockModules';
 mockModules();
 
 import { describe, expect, it } from '@jest/globals';
-import { GetPhotos, GetPhotosById } from '@src/api/export';
+import { GetPhotos, GetPhotosById, GetPhotosByMediaId } from '@src/api/export';
 import { initServer, stopServer } from '@src/server/server';
-import { addNPhotosToDb, expectToBeOk, getDataFromRet } from '@tests/helpers/functions';
+import {
+  addNPhotosToDb,
+  defaultPhoto,
+  expectToBeOk,
+  getDataFromRet,
+} from '@tests/helpers/functions';
 import * as sac from '@tests/helpers/setupAndCleanup';
 import { Express } from 'express';
 import { Perf } from '@tests/helpers/Perf';
@@ -44,10 +49,11 @@ describe('Test endpoints performance', () => {
     expect(data.endReached).toBe(true);
     expect(data.photos.length).toBe(1000);
 
+    console.log('getPhotos elapsed: ', elapsed);
     expect(elapsed).toBeLessThan(200);
   });
 
-  it.only("Test 'getPhotosById' endpoint performance", async () => {
+  it("Test 'getPhotosById' endpoint performance", async () => {
     const perf = new Perf();
 
     const retGetPhotos = await GetPhotos.Post({
@@ -79,7 +85,46 @@ describe('Test endpoints performance', () => {
 
     const allExist = data.photos.every(entry => entry.exists);
     expect(allExist).toBe(true);
-    console.log(elapsed);
+    console.log('getPhotosById elapsed: ', elapsed);
+    expect(elapsed).toBeLessThan(200);
+  });
+
+  it("Test 'getPhotosByMediaId' endpoint performance", async () => {
+    const perf = new Perf();
+
+    const retGetPhotos = await GetPhotos.Post({
+      number: 1000,
+      offset: 9000,
+      photoType: 'data',
+    });
+
+    if (!retGetPhotos.ok) {
+      throw new Error('Expected ok response');
+    }
+
+    const photosData = retGetPhotos.data.photos.map(photo => {
+      return { mediaId: photo.meta.mediaIds[0].mediaId };
+    });
+
+    perf.start();
+    const ret = await GetPhotosByMediaId.Post({
+      deviceUniqueId: defaultPhoto.deviceUniqueId,
+      photoType: 'data',
+      photosData,
+    });
+
+    const elapsed = perf.end();
+
+    expectToBeOk(ret);
+    expect(ret.warning).toBe(false);
+    const data = getDataFromRet(ret);
+
+    expect(data.number).toBe(1000);
+    expect(data.photos.length).toBe(1000);
+
+    const allExist = data.photos.every(entry => entry.exists);
+    expect(allExist).toBe(true);
+    console.log('getPhotosByMediaId elapsed: ', elapsed);
     expect(elapsed).toBeLessThan(200);
   });
 });
