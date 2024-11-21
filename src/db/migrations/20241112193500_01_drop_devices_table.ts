5e2dd41f94dea0cb2f8270773b663fac54e53770
Issam Benelgada <issambenelgada@gmail.com>
2024-11-21 21:42:50 +0100

    Added transactions to migrations.

--- src/db/migrations/20241112193500_01_drop_devices_table.ts
+++ src/db/migrations/20241112193500_01_drop_devices_table.ts
@@ -1,97 +1,134 @@
 import { DataTypes, QueryInterface } from 'sequelize';
 
 async function up({ context: queryInterface }: { context: QueryInterface }) {
-  await queryInterface.removeIndex('images', ['syncDate']);
+  await queryInterface.sequelize.transaction(async t => {
+    await queryInterface.removeIndex('images', ['syncDate']);
 
-  await queryInterface.addColumn('mediaIds', 'deviceUniqueId', {
-    type: DataTypes.STRING,
-    allowNull: false,
-    defaultValue: '',
+    await queryInterface.addColumn(
+      'mediaIds',
+      'deviceUniqueId',
+      {
+        type: DataTypes.STRING,
+        allowNull: false,
+        defaultValue: '',
+      },
+      { transaction: t },
+    );
+
+    await queryInterface.sequelize.query(
+      `
+      UPDATE "mediaIds" 
+      SET "deviceUniqueId" = "devices"."deviceUniqueId"
+      FROM "devices"
+      WHERE "mediaIds"."deviceId" = "devices"."id";
+    `,
+      { transaction: t },
+    );
+
+    await queryInterface.removeColumn('mediaIds', 'deviceId', { transaction: t });
+    await queryInterface.removeColumn('mediaIds', 'createdAt', { transaction: t });
+    await queryInterface.removeColumn('mediaIds', 'updatedAt', { transaction: t });
+
+    await queryInterface.removeIndex('mediaIds', ['mediaId'], { transaction: t });
+    await queryInterface.addIndex('mediaIds', ['imageId'], { transaction: t });
+
+    await queryInterface.changeColumn(
+      'mediaIds',
+      'imageId',
+      {
+        type: DataTypes.UUID,
+        references: {
+          model: 'images',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      { transaction: t },
+    );
+
+    await queryInterface.addConstraint('mediaIds', {
+      fields: ['mediaId', 'deviceUniqueId'],
+      type: 'unique',
+      name: 'unique_mediaId_deviceUniqueId',
+      transaction: t,
+    });
+
+    await queryInterface.dropTable('devices', { transaction: t });
   });
-
-  await queryInterface.sequelize.query(`
-    UPDATE "mediaIds" 
-    SET "deviceUniqueId" = "devices"."deviceUniqueId"
-    FROM "devices"
-    WHERE "mediaIds"."deviceId" = "devices"."id";
-  `);
-
-  await queryInterface.removeColumn('mediaIds', 'deviceId');
-  await queryInterface.removeColumn('mediaIds', 'createdAt');
-  await queryInterface.removeColumn('mediaIds', 'updatedAt');
-
-  await queryInterface.removeIndex('mediaIds', ['mediaId']);
-  await queryInterface.addIndex('mediaIds', ['imageId']);
-
-  await queryInterface.changeColumn('mediaIds', 'imageId', {
-    type: DataTypes.UUID,
-    references: {
-      model: 'images',
-      key: 'id',
-    },
-    onUpdate: 'CASCADE',
-    onDelete: 'CASCADE',
-  });
-
-  await queryInterface.addConstraint('mediaIds', {
-    fields: ['mediaId', 'deviceUniqueId'],
-    type: 'unique',
-    name: 'unique_mediaId_deviceUniqueId',
-  });
-
-  await queryInterface.dropTable('devices');
 }
 
 async function down({ context: queryInterface }: { context: QueryInterface }) {
-  await queryInterface.createTable('devices', {
-    id: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      allowNull: false,
-      unique: true,
-      primaryKey: true,
-    },
-    deviceUniqueId: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-    },
-    createdAt: {
-      type: DataTypes.DATE,
-      allowNull: false,
-    },
-    updatedAt: {
-      type: DataTypes.DATE,
-      allowNull: false,
-    },
+  await queryInterface.sequelize.transaction(async t => {
+    await queryInterface.createTable(
+      'devices',
+      {
+        id: {
+          type: DataTypes.UUID,
+          defaultValue: DataTypes.UUIDV4,
+          allowNull: false,
+          unique: true,
+          primaryKey: true,
+        },
+        deviceUniqueId: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          unique: true,
+        },
+        createdAt: {
+          type: DataTypes.DATE,
+          allowNull: false,
+        },
+        updatedAt: {
+          type: DataTypes.DATE,
+          allowNull: false,
+        },
+      },
+      { transaction: t },
+    );
+
+    await queryInterface.addIndex('devices', ['deviceUniqueId'], { transaction: t });
+
+    await queryInterface.removeIndex('mediaIds', ['imageId'], { transaction: t });
+    await queryInterface.addIndex('mediaIds', ['mediaId'], { transaction: t });
+
+    await queryInterface.removeColumn('mediaIds', 'deviceUniqueId', { transaction: t });
+
+    await queryInterface.addColumn(
+      'mediaIds',
+      'updatedAt',
+      {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      { transaction: t },
+    );
+    await queryInterface.addColumn(
+      'mediaIds',
+      'createdAt',
+      {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      { transaction: t },
+    );
+    await queryInterface.addColumn(
+      'mediaIds',
+      'deviceId',
+      {
+        type: DataTypes.UUID,
+        references: {
+          model: 'devices',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      },
+      { transaction: t },
+    );
+
+    await queryInterface.addIndex('images', ['syncDate'], { transaction: t });
   });
-
-  await queryInterface.addIndex('devices', ['deviceUniqueId']);
-
-  await queryInterface.removeIndex('mediaIds', ['imageId']);
-  await queryInterface.addIndex('mediaIds', ['mediaId']);
-
-  await queryInterface.removeColumn('mediaIds', 'deviceUniqueId');
-
-  await queryInterface.addColumn('mediaIds', 'updatedAt', {
-    type: DataTypes.DATE,
-    allowNull: false,
-  });
-  await queryInterface.addColumn('mediaIds', 'createdAt', {
-    type: DataTypes.DATE,
-    allowNull: false,
-  });
-  await queryInterface.addColumn('mediaIds', 'deviceId', {
-    type: DataTypes.UUID,
-    references: {
-      model: 'devices',
-      key: 'id',
-    },
-    onUpdate: 'CASCADE',
-    onDelete: 'SET NULL',
-  });
-
-  await queryInterface.addIndex('images', ['syncDate']);
 }
 
 const name = '20241112193500_01_drop_devices_table';
--- src/db/migrations/20241121172100_02_add_mediaIds_imageId_index.ts
+++ src/db/migrations/20241121172100_02_add_mediaIds_imageId_index.ts
@@ -1,11 +1,15 @@
 import { DataTypes, QueryInterface } from 'sequelize';
 
 async function up({ context: queryInterface }: { context: QueryInterface }) {
-  await queryInterface.addIndex('mediaIds', ['imageId']);
+  await queryInterface.sequelize.transaction(async t => {
+    await queryInterface.addIndex('mediaIds', ['imageId'], { transaction: t });
+  });
 }
 
 async function down({ context: queryInterface }: { context: QueryInterface }) {
-  await queryInterface.removeIndex('mediaIds', ['imageId']);
+  await queryInterface.sequelize.transaction(async t => {
+    await queryInterface.removeIndex('mediaIds', ['imageId'], { transaction: t });
+  });
 }
 
 const name = '20241121172100_02_add_mediaIds_imageId_index';
