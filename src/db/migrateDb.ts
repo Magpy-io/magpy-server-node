ed8b6f31f0bf82feee95d9dc051d520dd3151cb4
Issam Benelgada <issambenelgada@gmail.com>
2024-11-21 17:27:28 +0100

    Added new migration to add missing index in mediaIds table.

--- src/db/migrateDb.ts
+++ src/db/migrateDb.ts
@@ -4,10 +4,11 @@ import { SequelizeStorage, Umzug } from 'umzug';
 
 import initial from './migrations/20241112183000_00_initial';
 import drop_devices_table from './migrations/20241112193500_01_drop_devices_table';
+import add_mediaIds_imageId_index from './migrations/20241121172100_02_add_mediaIds_imageId_index';
 
 export async function migrateDb(sequelize: Sequelize) {
   const umzug = new Umzug({
-    migrations: [initial, drop_devices_table],
+    migrations: [initial, drop_devices_table, add_mediaIds_imageId_index],
     context: sequelize.getQueryInterface(),
     storage: new SequelizeStorage({ sequelize }),
     logger: Logger.child({ source: 'umzug' }),
